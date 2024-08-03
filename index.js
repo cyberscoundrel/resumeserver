1d17713394b9893db1b6c7d8083cf609ef10cc59
cyberscoundrel <cyberscoundrel@gmail.com>
2024-08-03 06:27:25 -0500

    changed port

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
+const fs = require('fs')
 const express = require('express')
 const app = express()
 const port = 80
 const path = require('path')
 
+var https = require('https');
+var privateKey  = fs.readFileSync('./private.key', 'utf8');
+var certificate = fs.readFileSync('./certificate.crt', 'utf8');
+
+var credentials = {key: privateKey, cert: certificate};
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'))
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+var httpsServer = https.createServer(credentials, app);
+httpsServer.listen(443)
